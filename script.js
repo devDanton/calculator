cd4fc6e5e39d7136abe094cc40db5d5f1100f2aa
Danton da Rosa Abreu <77130559+devDanton@users.noreply.github.com>
2023-02-08 23:37:21 -0300

    add responsive and script

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ window.addEventListener('resize', () => {
   let vh = window.innerHeight * 0.01
   document.documentElement.style.setProperty('--vh', `${vh}px`)
 })
+var expression = document.getElementById('expression')
+var buttons = document.getElementsByTagName('input')
+var resultH1 = document.getElementById('resultado')
+
+for (var i = 0; i < buttons.length; i++) {
+  if (buttons[i].type === 'button') {
+    buttons[i].addEventListener('click', function () {
+      if (this.value === '=') {
+        resultH1.value = eval(expression.value)
+      } else if (this.value === 'C') {
+        expression.value = ''
+      } else {
+        expression.value += this.value
+      }
+      console.log(this.value)
+    })
+  }
+}
