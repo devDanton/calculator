6253fe79841ffdcd43d3f759a4f8f240ec1af073
Danton da Rosa Abreu <77130559+devDanton@users.noreply.github.com>
2023-02-12 22:47:46 -0300

    Corr perct

--- script.js
+++ script.js
@@ -15,9 +15,37 @@ function clean() {
 }
 
 function percent() {
-  var numero = document.getElementById("resultado").innerHTML
-  document.getElementById("resultado").innerHTML = numero * num / 100
+  let expressao = document.getElementById("resultado").innerHTML
+  var values = expressao.split(/[\+\-\*\/\%\(\)]/)
+  var firstValue = parseFloat(values[0])
+  var secondValue = parseFloat(values[1])
+  var operator = expressao.match(/[\+\-\*\/\%\(\)]/)
+
+  let percentual = eval(firstValue * secondValue / 100)
+
+  let resultado
+
+  if (operator == "+") {
+    resultado = firstValue + percentual;
+  } else if (operator == "-") {
+    resultado = firstValue - percentual;
+  } else if (operator == "*") {
+    resultado = firstValue * percentual;
+  } else if (operator == "/") {
+    resultado = firstValue / percentual;
+  } else {
+    resultado = "Operação inválida";
+  }
+
+  console.log("Primeiro valor: " + firstValue)
+  console.log("Segundo valor: " + secondValue)
+  console.log("operador: " + operator)
+  console.log("percentual: " + percentual)
+  console.log("Resultado: " + resultado)
+
+  document.getElementById("resultado").innerHTML = resultado
 }
+
 
 function back() {
   var resultado = document.getElementById("resultado").innerHTML
